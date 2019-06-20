a6f82d80e1a5f0d5f9907406842128574e8102fc
DanBlakeNZ <dblakenz@hotmail.com>
2019-06-20 15:06:11 +1000

    added styles

---
+++ frontend/pages/_document.js
@@ -0,0 +1,29 @@
+// Ensures styles are ready on initial page load - fixes issue where can appear without styles on initial load.
+// https://www.styled-components.com/docs/advanced#nextjs
+// https://nextjs.org/docs#custom-document
+
+import Document, { Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>{this.props.styleTags}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
